3c2a714839e9539b53e6ca1ca54f9a3a64ac765b
SatganzDevs <satganzdevs@gmail.com>
2023-12-19 01:01:01 +0000

    anjasss

--- script.js
+++ script.js
@@ -4,3 +4,37 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+const statusElement = document.getElementById('status');
+const modalElement = document.getElementById('myModal');
+const modalStatusElement = document.getElementById('modalStatus');
+let reconnecting = false;
+
+function updateStatus() {
+  if (navigator.onLine) {
+    if (reconnecting) {
+      showModal('Reconnected');
+      reconnecting = false;
+    }
+  } else {
+    statusElement.textContent = 'Offline';
+    statusElement.classList.remove('online');
+    statusElement.classList.add('offline');
+    showModal('Offline');
+    reconnecting = true;
+  }
+}
+
+function showModal(status) {
+  modalStatusElement.textContent = status;
+  modalElement.style.display = 'flex';
+}
+
+function closeModal() {
+  modalElement.style.display = 'none';
+}
+
+
+window.addEventListener('offline', updateStatus);
+
+// Initial status check
+updateStatus();
